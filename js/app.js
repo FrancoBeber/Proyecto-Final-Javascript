e17718042d44f81cfd3c4026d8102a5dacec619c
Franco Beber <fbeber73@gmail.com>
2022-11-09 18:22:16 -0300

    agrego botones al carro para aum o quit prod

--- js/app.js
+++ js/app.js
@@ -4,15 +4,16 @@ let inventario = [];
 let contadorCarro = 0;
 
 /* Agregamos objetos al inventario */
-inventario.push(new Item("1", "Monitor", "Samsung", "Modelo1", 57000));
-inventario.push(new Item("2", "Monitor", "Samsung", "Modelo1", 57000));
-inventario.push(new Item("3", "Monitor", "Samsung", "Modelo1", 57000));
-inventario.push(new Item("4", "Monitor", "Samsung", "Modelo1", 57000));
-inventario.push(new Item("5", "Monitor", "Samsung", "Modelo1", 57000));
-inventario.push(new Item("6", "Monitor", "Samsung", "Modelo1", 57000));
-inventario.push(new Item("7", "Monitor", "Samsung", "Modelo1", 57000));
-inventario.push(new Item("8", "Monitor", "Samsung", "Modelo1", 57000));
+inventario.push(new Item("1", "Monitor", "Samsung", "Modelo1", 57000, 1));
+inventario.push(new Item("2", "Monitor", "Samsung", "Modelo1", 57000, 1));
+inventario.push(new Item("3", "Monitor", "Samsung", "Modelo1", 57000, 1));
+inventario.push(new Item("4", "Monitor", "Samsung", "Modelo1", 57000, 1));
+inventario.push(new Item("5", "Monitor", "Samsung", "Modelo1", 57000, 1));
+inventario.push(new Item("6", "Monitor", "Samsung", "Modelo1", 57000, 1));
+inventario.push(new Item("7", "Monitor", "Samsung", "Modelo1", 57000, 1));
+inventario.push(new Item("8", "Monitor", "Samsung", "Modelo1", 57000, 1));
 
+/* Creamos las cards con los datos del inventario */
 const productoCatalogoHTML = (item) => {
   return `
     <div class="col col-xl-3 col-md-6 col-sm-12 py-5">
@@ -28,6 +29,8 @@ const productoCatalogoHTML = (item) => {
             </div>
           </div>`;
 };
+
+/* Creamos las cards con los productos agregados al carrito */
 const productoCarritoHTML = (item) => {
   return `
     <div class="col col-xl-3 col-md-6 col-sm-12 py-5">
@@ -38,12 +41,16 @@ const productoCarritoHTML = (item) => {
                 <p class="card-text">Marca: ${item.marca}</p>
                 <p class="card-text">Modelo: ${item.modelo}</p>
                 <p class="card-text">Precio: ${item.precio}</p>
+                <p class="card-text">Cantidad: ${item.cantidad}</p>
                 <button id="btn-carrito-${item.idCompra}" class="btn btn-danger">Quitar</button>
+                <button id="btn-sumcant-${item.idCompra}" class="btn btn-danger">+</button>
+                <button id="btn-rescant-${item.idCompra}" class="btn btn-danger">-</button>
               </div>
             </div>
           </div>`;
 };
 
+/* Funcion que nos permite mostrar todos los producots */
 const mostrarCatalogo = () => {
   let tarjetaCatalogo = document.getElementById("catalogo");
   let catalogoHTML = "";
@@ -55,6 +62,8 @@ const mostrarCatalogo = () => {
   tarjetaCatalogo.innerHTML = catalogoHTML;
   botonesCatalogo();
 };
+
+/* Funcion para mostrar elementos en el carro */
 const mostrarCarrito = () => {
   let cardCarro = document.getElementById("carrito");
   let precioCarro = document.getElementById("precioTotal");
@@ -62,14 +71,17 @@ const mostrarCarrito = () => {
   let precioFinal = 0;
   for (let item of carrito) {
     carroHTML += productoCarritoHTML(item);
-    precioFinal += item.precio;
+    precioFinal = precioFinal + item.precio * item.cantidad;
   }
 
   precioCarro.innerHTML = precioFinal;
   cardCarro.innerHTML = carroHTML;
   botonesCarrito();
+  botonSumarCant();
+  botonRestarCant();
 };
 
+/* Funcion para agregar listener a los botones de agregar al carro */
 const botonesCatalogo = () => {
   for (let item of inventario) {
     let idBoton = `btn-catalogo-${item.id}`;
@@ -82,13 +94,17 @@ const botonesCatalogo = () => {
         modelo: item.modelo,
         precio: item.precio,
         idCompra: contadorCarro,
+        cantidad: item.cantidad,
       };
       contadorCarro++;
       carrito.push(itemCarrito);
       mostrarCarrito();
+      localStorage.setItem("carrito", JSON.stringify(carrito));
     });
   }
 };
+
+/* Funcion para agregar listener a los botones de quitar del carro */
 const botonesCarrito = () => {
   for (let item of carrito) {
     let idBtnCarro = `btn-carrito-${item.idCompra}`;
@@ -100,10 +116,42 @@ const botonesCarrito = () => {
       );
       carrito.splice(indice, 1);
       mostrarCarrito();
+      localStorage.setItem("carrito", JSON.stringify(carrito));
     });
   }
 };
 
+const botonSumarCant = () => {
+  for (let item of carrito) {
+    let idBtnSum = `btn-sumcant-${item.idCompra}`;
+    let cardSumar = document.getElementById(idBtnSum);
+
+    cardSumar.addEventListener("click", () => {
+      let indice = carrito.findIndex((p) => p.idCompra == item.idCompra);
+      carrito[indice].cantidad++;
+      mostrarCarrito();
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+    });
+  }
+};
+
+const botonRestarCant = () => {
+  for (let item of carrito) {
+    let idBtnRes = `btn-rescant-${item.idCompra}`;
+    let cardRestar = document.getElementById(idBtnRes);
+
+    cardRestar.addEventListener("click", () => {
+      let indice = carrito.findIndex((p) => p.idCompra == item.idCompra);
+      if (carrito[indice].cantidad > 0) {
+        carrito[indice].cantidad--;
+      }
+      mostrarCarrito();
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+    });
+  }
+};
+
+/* Ejecucion del programa */
 mostrarCatalogo();
 
 console.log(productoCatalogoHTML(inventario[0]));
--- js/itemClass.js
+++ js/itemClass.js
@@ -5,12 +5,14 @@ class Item {
   modelo;
   precio;
   idCompra;
+  cantidad;
 
-  constructor(id, tipo, marca, modelo, precio) {
+  constructor(id, tipo, marca, modelo, precio, cantidad) {
     this.id = id;
     this.tipo = tipo;
     this.marca = marca;
     this.modelo = modelo;
     this.precio = precio;
+    this.cantidad = cantidad;
   }
 }
